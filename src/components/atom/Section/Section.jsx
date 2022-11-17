6e21a595b17ae1ce74e2d9f442afbfd095ab2063
Parkjju <rudwns3927@gmail.com>
2022-11-17 18:21:05 +0900

    style: 섹션 컴포넌트 포지션 수정

--- src/components/atom/Section/Section.jsx
+++ src/components/atom/Section/Section.jsx
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 export default styled.div`
     display: flex;
     flex-direction: row;
-    width: 100%;
-    height: 600px;
     background-color: ${(props) => (props.bgColor ? `${props.bgColor}` : null)};
+    position: relative;
+    left: 0;
+    margin: 0 -64px;
 `;
