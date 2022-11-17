c6870169a2af02f460dd0b10e61b742696d21e5f
Parkjju <rudwns3927@gmail.com>
2022-11-17 18:22:04 +0900

    style: 홈 컴포넌트 분리

---
+++ src/components/pages/Home/Home.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+export const ImageBox = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 0 64px;
+`;
+
+export const SectionImage = styled.img`
+    width: 50%;
+`;
