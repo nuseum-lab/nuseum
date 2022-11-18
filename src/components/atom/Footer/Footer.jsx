9cc76fea373b77d734b12f32ef7be658bf4df24b
Parkjju <rudwns3927@gmail.com>
2022-11-18 14:06:51 +0900

    feat: Footer 컴포넌트 생성 및 portal 등록

---
+++ src/components/atom/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default styled.footer`
+    height: 80px;
+    margin: 0 64px;
+    margin-top: 50px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-top: 1px solid black;
+`;
