98367b1f4ba9d6cee40da51e29bccfcb034cf6ec
Parkjju <rudwns3927@gmail.com>
2022-11-21 11:49:48 +0900

    feat: 헤더 컴포넌트 isOpen 프롭스 등록 및 GNB컴포넌트 전달 / Header.styled.js isOpen프롭스 스타일링 분기

--- src/components/organism/Header/Header.jsx
+++ src/components/organism/Header/Header.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
+import { colorPalette } from '../../../lib/styles/colorPalette';
 import Text from '../../atom/Text/Text';
 import GNB from '../../molecules/GNB';
 import { Box } from './Header.styled';
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <Box>
+        <Box isOpen={isOpen}>
             <Text
                 fontStyle={{
                     fontSize: 'logo',
@@ -13,7 +16,22 @@ const Header = () => {
             >
                 NUSEUM
             </Text>
-            <GNB />
+
+            <GNB setIsOpen={setIsOpen} isOpen={isOpen} />
+
+            {isOpen ? (
+                <div
+                    style={{
+                        position: 'fixed',
+                        top: 100,
+                        left: 0,
+                        zIndex: 999,
+                        width: '100vw',
+                        height: '100vh',
+                        backgroundColor: colorPalette.whiteOrange,
+                    }}
+                ></div>
+            ) : null}
         </Box>
     );
 };
--- src/components/organism/Header/Header.styled.js
+++ src/components/organism/Header/Header.styled.js
@@ -1,9 +1,18 @@
 import styled from 'styled-components';
+import { colorPalette } from '../../../lib/styles/colorPalette';
 
 export const Box = styled.div`
     width: 100%;
     display: flex;
     flex-direction: row;
     align-items: center;
+    left: 0;
+    z-index: 999;
     justify-content: space-between;
+    margin: 0 auto;
+    position: ${(props) => (props.isOpen ? 'fixed' : null)};
+    height: ${(props) => (props.isOpen ? '114px' : null)};
+    background-color: ${colorPalette.whiteOrange};
+    padding: ${(props) => (props.isOpen ? '0 64px' : null)};
+    box-sizing: border-box;
 `;
