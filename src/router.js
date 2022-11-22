536cd9b88e4068ae3edc19aa63c523351aa0e8dc
Parkjju <rudwns3927@gmail.com>
2022-11-22 10:35:16 +0900

    feat: RouteChangeTracker 컴포넌트 생성 및 Router 로케이션 등록

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import RouteChangeTracker from './components/atom/RouteChangeTracker';
 import Home from './components/pages/Home/Home';
 
 const Router = () => {
     return (
         <BrowserRouter>
+            <RouteChangeTracker />
             <Routes>
                 <Route element={<Home />} path='/' />
             </Routes>
