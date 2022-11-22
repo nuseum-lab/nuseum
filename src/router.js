42ae8bb9083504f26cfbf20b0b67c1db06c996af
Parkjju <rudwns3927@gmail.com>
2022-11-22 11:07:19 +0900

    feat: 404페이지 임시 라우터 등록

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RouteChangeTracker from './components/atom/RouteChangeTracker';
 import Home from './components/pages/Home/Home';
+import NotFound from './components/pages/NotFound';
 
 const Router = () => {
     return (
@@ -8,6 +9,7 @@ const Router = () => {
             <RouteChangeTracker />
             <Routes>
                 <Route element={<Home />} path='/' />
+                <Route path={'*'} component={NotFound} />
             </Routes>
         </BrowserRouter>
     );
