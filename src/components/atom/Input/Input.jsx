3ffdcdd9bf113047b64694808c1371bacf15887d
Parkjju <rudwns3927@gmail.com>
2022-11-24 16:00:16 +0900

    feat: Input컴포넌트 생성

---
+++ src/components/atom/Input/Input.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default styled.input`
+    box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    border: none;
+    height: 60px;
+    outline: none;
+    padding: 10px;
+    font-size: 16px;
+    text-align: center;
+`;
