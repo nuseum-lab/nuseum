ca7746cd62a998924963a957802beb0c7b03be06
Parkjju <rudwns3927@gmail.com>
2022-11-25 09:40:15 +0900

    style: Question페이지 반응형 대응 - flexdirection column-reverse정렬 및 width조정

--- src/components/pages/Question/Question.styled.js
+++ src/components/pages/Question/Question.styled.js
@@ -5,7 +5,70 @@ export const Img = styled.img`
     object-fit: cover;
     width: 100%;
     height: 80%;
-    object-position: -20rem 0;
+    object-position: 70% 0;
+
+    @media all and (max-width: 1135px) and (min-width: 600px) {
+        object-position: 70% 0;
+    }
+    @media all and (max-width: 599px) {
+        width: 100%;
+    }
+`;
+export const BackgroundImg = styled.img`
+    display: block;
+    box-sizing: border-box;
+    object-fit: cover;
+    width: 50%;
+    height: 100%;
+    z-index: -1;
+    position: absolute;
+    @media all and (max-width: 599px) {
+        width: 100%;
+        height: 50%;
+    }
+`;
+export const UtilWrapper = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    @media all and (max-width: 599px) {
+        width: 100%;
+    }
+`;
+export const UtilLayout = styled.div`
+    height: 50%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media all and (max-width: 599px) {
+        height: 80%;
+    }
+`;
+
+export const ImgWrapper = styled.div`
+    width: 50%;
+    height: 100%;
+    overflow: hidden;
+    display: flex;
+    align-items: flex-end;
+
+    @media all and (max-width: 599px) {
+        width: 100%;
+    }
+`;
+
+export const BackgroundWrapper = styled.div`
+    box-sizing: border-box;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    @media all and (max-width: 599px) {
+        flex-direction: column-reverse;
+    }
 `;
 
 export const TextWrapper = styled.div`
