cacaab2240a0494d1038c1442bb386a2bed3656f
Parkjju <rudwns3927@gmail.com>
2022-11-29 16:40:14 +0900

    feat: Today 컴포넌트 제작, 퍼블리싱 및 API연동 전 작업

--- src/components/pages/Today/Today.jsx
+++ src/components/pages/Today/Today.jsx
@@ -1,9 +1,302 @@
-import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import background from '../../../lib/assets/question-background.png';
+import search from '../../../lib/assets/search.png';
+import { fontSize } from '../../../lib/styles/fontSize';
+import Button from '../../atom/Button';
+import Input from '../../atom/Input/Input';
+import Text from '../../atom/Text/Text';
+import Header from '../../organism/Header/Header';
+import Layout from '../Layout';
+import {
+    BackgroundImg,
+    BackgroundWrapper,
+    Img,
+    ImgWrapper,
+    TextWrapper,
+    UtilLayout,
+    UtilWrapper,
+} from '../Question/Question.styled';
+import { SelectButton, SelectButtonWrapper } from './Today.styled';
 
 const Today = () => {
+    const navigate = useNavigate();
     const location = useLocation();
     console.log(location.state);
+    const [selected, setSelected] = useState({
+        breakfast: false,
+        lunch: false,
+        dinner: false,
+        snack: false,
+        supplement: false,
+    });
+    const [food, setFood] = useState('');
+    const [foodList, setFoodList] = useState({
+        breakfast: [],
+        lunch: [],
+        dinner: [],
+        snack: [],
+        supplement: [],
+    });
 
-    return <h1>TODAY!</h1>;
+    return (
+        <Layout style={{ width: '100%', padding: 0 }}>
+            <Header />
+            <BackgroundWrapper>
+                <ImgWrapper
+                    style={{
+                        alignItems: 'center',
+                    }}
+                >
+                    <UtilLayout
+                        style={{
+                            height: '70%',
+                            overflowY: 'scroll',
+                        }}
+                    >
+                        <TextWrapper style={{ width: '80%' }}>
+                            {Object.keys(foodList).map((item) =>
+                                foodList[item].length > 0 ? (
+                                    <>
+                                        <Text
+                                            key={item}
+                                            style={{
+                                                paddingLeft: 5,
+                                                margin: '20px 0',
+                                                width: '80%',
+                                                textAlign: 'left',
+                                                color: 'white',
+                                                borderBottom: '2px solid white',
+                                                lineHeight: 1.5,
+                                                fontSize: 32,
+                                                fontWeight: 600,
+                                            }}
+                                        >
+                                            {item === 'breakfast'
+                                                ? '아침'
+                                                : item === 'lunch'
+                                                ? '점심'
+                                                : item === 'dinner'
+                                                ? '저녁'
+                                                : item === 'snack'
+                                                ? '간식'
+                                                : item === 'supplement'
+                                                ? '영양제'
+                                                : null}
+                                        </Text>
+                                        {foodList[item].map((meal, index) => (
+                                            <Text
+                                                key={index + item}
+                                                style={{
+                                                    paddingLeft: 5,
+                                                    marginBottom: '12px',
+                                                    width: '80%',
+                                                    textAlign: 'left',
+                                                    color: 'white',
+                                                    borderBottom:
+                                                        '0.5px solid white',
+                                                    lineHeight: 1.5,
+                                                    fontSize: 24,
+                                                    fontWeight: 400,
+                                                }}
+                                            >
+                                                {meal}
+                                            </Text>
+                                        ))}
+                                    </>
+                                ) : null
+                            )}
+                        </TextWrapper>
+                        <Button
+                            style={{
+                                marginTop: 30,
+                                fontSize: fontSize.button,
+                                position: 'absolute',
+                                bottom: 30,
+                                display: 'block',
+                            }}
+                            onClick={() =>
+                                navigate('/analysis', {
+                                    state: {
+                                        age: location.state.age,
+                                        gender: location.state.gender,
+                                    },
+                                })
+                            }
+                        >
+                            분석하기
+                        </Button>
+                    </UtilLayout>
+
+                    <BackgroundImg src={background} alt='background' />
+                </ImgWrapper>
+
+                <UtilWrapper>
+                    <UtilLayout>
+                        <TextWrapper>
+                            <Text
+                                fontStyle={{
+                                    fontWeight: 'primary',
+                                    fontSize: 'subtitle',
+                                }}
+                                style={{ marginBottom: '12px' }}
+                            >
+                                TODAY
+                            </Text>
+                            <Text
+                                fontStyle={{
+                                    fontWeight: 'normal',
+                                    fontSize: 'normal',
+                                }}
+                            >
+                                오늘 먹은 음식을 입력해주세요 :)
+                            </Text>
+                        </TextWrapper>
+
+                        <SelectButtonWrapper style={{ marginBottom: 30 }}>
+                            <SelectButton
+                                selected={selected.breakfast}
+                                onClick={() =>
+                                    setSelected({
+                                        breakfast: true,
+                                        lunch: false,
+                                        dinner: false,
+                                        snack: false,
+                                        supplement: false,
+                                    })
+                                }
+                                as='button'
+                            >
+                                아침
+                            </SelectButton>
+                            <SelectButton
+                                selected={selected.lunch}
+                                onClick={() =>
+                                    setSelected({
+                                        breakfast: false,
+                                        lunch: true,
+                                        dinner: false,
+                                        snack: false,
+                                        supplement: false,
+                                    })
+                                }
+                                as='button'
+                            >
+                                점심
+                            </SelectButton>
+                            <SelectButton
+                                selected={selected.dinner}
+                                onClick={() =>
+                                    setSelected({
+                                        breakfast: false,
+                                        lunch: false,
+                                        dinner: true,
+                                        snack: false,
+                                        supplement: false,
+                                    })
+                                }
+                                as='button'
+                            >
+                                저녁
+                            </SelectButton>
+                            <SelectButton
+                                selected={selected.snack}
+                                onClick={() =>
+                                    setSelected({
+                                        breakfast: false,
+                                        lunch: false,
+                                        dinner: false,
+                                        snack: true,
+                                        supplement: false,
+                                    })
+                                }
+                                as='button'
+                            >
+                                간식
+                            </SelectButton>
+                            <SelectButton
+                                selected={selected.supplement}
+                                onClick={() =>
+                                    setSelected({
+                                        breakfast: false,
+                                        lunch: false,
+                                        dinner: false,
+                                        snack: false,
+                                        supplement: true,
+                                    })
+                                }
+                                as='button'
+                            >
+                                영양제
+                            </SelectButton>
+                        </SelectButtonWrapper>
+
+                        <Text
+                            fontStyle={{
+                                fontWeight: 'normal',
+                                fontSize: 'normal',
+                            }}
+                            style={{ marginBottom: 20 }}
+                        >
+                            찾고 싶은 음식을 검색해주세요.
+                        </Text>
+                        <div
+                            style={{
+                                width: '80%',
+                                display: 'flex',
+                                flexDirection: 'row',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Input
+                                style={{
+                                    width: '80%',
+                                    textAlign: 'left',
+                                    paddingLeft: 30,
+                                    paddingRight: 50,
+                                    marginBottom: 0,
+                                }}
+                                onChange={(e) => {
+                                    setFood(e.target.value);
+                                }}
+                                onKeyPress={(e) => {
+                                    if (e.target.value === '') {
+                                        return;
+                                    }
+
+                                    if (e.key === 'Enter') {
+                                        for (let key in selected) {
+                                            if (selected[key]) {
+                                                setFoodList((prev) => {
+                                                    return {
+                                                        ...prev,
+                                                        [key]: [
+                                                            ...prev[key],
+                                                            food,
+                                                        ],
+                                                    };
+                                                });
+                                            }
+                                        }
+                                    }
+                                }}
+                            />
+                            <Img
+                                src={search}
+                                style={{
+                                    width: 20,
+                                    height: 20,
+                                    position: 'relative',
+                                    objectPosition: null,
+                                    right: 40,
+                                }}
+                            />
+                        </div>
+                    </UtilLayout>
+                </UtilWrapper>
+            </BackgroundWrapper>
+        </Layout>
+    );
 };
 export default Today;
