17e664ec3a847fd171a7e1a719c0f4764fd60be6
Parkjju <rudwns3927@gmail.com>
2022-11-30 16:29:17 +0900

    style: Analysis 반응형 스타일 추가

--- src/components/pages/Analysis/Analysis.styled.js
+++ src/components/pages/Analysis/Analysis.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Title = styled.div`
-    width: '100%';
+    width: 100%;
     display: flex;
     justify-content: center;
     flex-direction: column;
@@ -10,14 +10,18 @@ export const Title = styled.div`
 `;
 
 export const PointLayout = styled.div`
-    height: 0;
-    width: 80%;
-    padding-bottom: 80%;
+    width: 100%;
+    height: 50%;
     background-color: rgba(93, 78, 68, 0.1);
     border-radius: 10px;
     display: flex;
-    flex-direction: column;
     align-items: center;
+    flex-direction: column;
+    justify-content: space-around;
+    @media all and (max-width: 768px) {
+        width: 80%;
+        padding-top: 30px;
+    }
 `;
 
 export const PointBox = styled.div`
@@ -25,11 +29,29 @@ export const PointBox = styled.div`
     grid-template-rows: repeat(3, 1fr);
     grid-template-columns: repeat(3, 1fr);
     justify-items: center;
-    align-items: center;
+    align-items: flex-start;
+    height: 80%;
+    @media all and (max-width: 768px) {
+        margin-top: 30px;
+    }
 `;
 
 export const Point = styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: space-between;
     align-items: center;
+    width: 80%;
+    height: 70%;
+    @media all and (max-width: 768px) {
+        margin-bottom: 30px;
+    }
+`;
+
+export const PointImg = styled.img`
+    width: 40%;
+    @media all and (max-width: 768px) {
+        width: 30%;
+        margin-bottom: 10px;
+    }
 `;
