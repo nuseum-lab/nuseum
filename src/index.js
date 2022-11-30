39776f862266b4612a10a90b0d4ac6c36c009900
Parkjju <rudwns3927@gmail.com>
2022-11-30 10:27:10 +0900

    feat: axios 설치 및 기본설정 추가

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import ReactGA from 'react-ga4';
+import axios from 'axios';
+
 const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
 ReactGA.initialize(TRACKING_ID);
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
