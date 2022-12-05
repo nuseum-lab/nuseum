d00beed349fa41f60beafad09eb31434dedd15c4
Parkjju <rudwns3927@gmail.com>
2022-12-05 09:43:47 +0900

    feat: baseURL 설정

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import axios from 'axios';
 const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
 ReactGA.initialize(TRACKING_ID);
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/';
-
+axios.defaults.baseURL = 'https://www.nuseum.site/api/v1';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
