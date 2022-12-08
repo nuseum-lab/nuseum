d54b4d884a7851e41f669f068a709ebc2a10c24b
Parkjju <rudwns3927@gmail.com>
2022-12-08 10:37:52 +0900

    feat: 사이트 영문화 store provider 등록

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import ReactGA from 'react-ga4';
 import axios from 'axios';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
 ReactGA.initialize(TRACKING_ID);
 
 axios.defaults.baseURL = 'https://www.nuseum.site/api/v1';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
