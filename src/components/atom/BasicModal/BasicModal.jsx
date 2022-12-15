659f951794f29a417c0142854240e0b7acc84a82
Parkjju <rudwns3927@gmail.com>
2022-12-15 09:17:13 +0900

    feat: 221215 BasicModal 제작

---
+++ src/components/atom/BasicModal/BasicModal.jsx
@@ -0,0 +1,96 @@
+import Button from '@mui/joy/Button';
+import TextField from '@mui/joy/TextField';
+import Modal from '@mui/joy/Modal';
+import ModalDialog from '@mui/joy/ModalDialog';
+import Stack from '@mui/joy/Stack';
+import Typography from '@mui/joy/Typography';
+import React, { useState } from 'react';
+
+const BasicModal = ({ open, inputTitle, setOpen, setFoodList, selected }) => {
+    const [amount, setAmount] = useState(0);
+
+    return (
+        <React.Fragment>
+            <Modal open={open}>
+                <ModalDialog
+                    aria-labelledby='basic-modal-dialog-title'
+                    aria-describedby='basic-modal-dialog-description'
+                    sx={{
+                        maxWidth: 500,
+                        borderRadius: 'md',
+                        p: 3,
+                        boxShadow: 'lg',
+                        backgroundColor: 'white',
+                    }}
+                >
+                    <div
+                        style={{
+                            width: '100%',
+                            display: 'flex',
+                            justifyContent: 'flex-start',
+                            marginBottom: 20,
+                        }}
+                    >
+                        <span
+                            style={{
+                                cursor: 'pointer',
+                            }}
+                            onClick={() => setOpen(false)}
+                            className='material-symbols-outlined'
+                        >
+                            close
+                        </span>
+                    </div>
+                    <Typography
+                        id='basic-modal-dialog-title'
+                        component='h2'
+                        level='inherit'
+                        fontSize='1.25em'
+                        mb='0.25em'
+                    >
+                        {inputTitle}
+                    </Typography>
+                    <Typography
+                        id='basic-modal-dialog-description'
+                        mt={0.5}
+                        mb={2}
+                        textColor='text.tertiary'
+                    >
+                        g 또는 ml을 입력해주세요.
+                    </Typography>
+                    <form
+                        onSubmit={(event) => {
+                            event.preventDefault();
+                            for (let key in selected) {
+                                if (selected[key]) {
+                                    setFoodList((prev) => {
+                                        return {
+                                            ...prev,
+                                            [key]: [
+                                                ...prev[key],
+                                                [inputTitle, amount],
+                                            ],
+                                        };
+                                    });
+                                }
+                            }
+
+                            setOpen(false);
+                        }}
+                    >
+                        <Stack spacing={2}>
+                            <TextField
+                                onChange={(e) => setAmount(e.target.value)}
+                                label='섭취 중량'
+                                autoFocus
+                                required
+                            />
+                            <Button type='submit'>Submit</Button>
+                        </Stack>
+                    </form>
+                </ModalDialog>
+            </Modal>
+        </React.Fragment>
+    );
+};
+export default BasicModal;
