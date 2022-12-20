1d956c126e94ae3b8bf308abe62d3ba0e9420b27
Parkjju <rudwns3927@gmail.com>
2022-12-20 17:19:05 +0900

    feat: 리듀서 등록

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import languageSlice from './language-slice';
+import nutritionSlice from './nutrition-slice';
 
 const store = configureStore({
     reducer: {
         language: languageSlice,
+        nutrition: nutritionSlice,
     },
 });
 
